6a26052279c8ef0768c7bb739b90e24719b8c057
Xshatriya <xshatriya777@gmail.com>
2026-08-15 16:13:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -638,7 +638,7 @@ function addContactRow(data = {}) {
     tr.innerHTML = `
         <td style="text-align: center;"><input type="text" class="contact-name" value="${data.name || ''}" placeholder="이름" style="width: 50px; min-width: 30px; text-align: center;" ${disabledAttr}></td>
         <td style="text-align: center;"><input type="text" class="contact-role" value="${data.role || ''}" placeholder="직책" style="width: 50px; min-width: 30px; text-align: center;" ${disabledAttr}></td>
-        <td style="text-align: center;"><input type="text" class="contact-phone" value="${formatPhoneNumber(data.phone || '')}" placeholder="연락처" style="width: 70px; min-width: 70px; text-align: center;" onchange="this.value = formatPhoneNumber(this.value)" ${disabledAttr}></td>
+        <td style="text-align: center;"><input type="text" class="contact-phone" value="${formatPhoneNumber(data.phone || '')}" placeholder="연락처" style="width: 100px; min-width: 100px; text-align: center;" onchange="this.value = formatPhoneNumber(this.value)" ${disabledAttr}></td>
         <td style="text-align: center;"><button class="btn-call" onclick="callContact(this)" style="background-color: #10b981; color: white; padding: 5px 8px; font-size: 11px; border: none; border-radius: 4px; cursor: pointer;">통화</button></td>
         <td><input type="text" class="contact-memo" value="${data.memo || ''}" placeholder="메모" style="width: 100%; min-width: 200px;" ${disabledAttr}></td>
         <td style="text-align: center;"><button class="btn-copy" onclick="copyContactInfo(this)">복사</button></td>
